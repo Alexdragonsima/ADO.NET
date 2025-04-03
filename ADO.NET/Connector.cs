79d4c7052b9807d9e1d5417dd73072ef854466c4
Алексей Семионов <alehaaaaa@mail.ru>
2025-04-03 14:00:11 +0300

    INSERT DONE

--- ADO.NET/Connector.cs
+++ ADO.NET/Connector.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Diagnostics;
 
 namespace ADO.NET
 {
@@ -34,7 +36,7 @@ namespace ADO.NET
 		{
 			Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s', first_name,last_name)", "Movies,Directors", "director=director_id");
 		}
-		public static void Select(string colums, string tables, string condition = null)
+		static void Select(string colums, string tables, string condition = null)
 		{
 			//3) Создаем команлу ,которую нужно выполнить насервере:
 			//string cmd = "SELECT title, release_date, FORMATMESSAGE(N'%s %s', first_name, last_name) FROM Movies, Directors  WHERE director = director_id";
@@ -75,11 +77,49 @@ namespace ADO.NET
 
 		public static void InsertDirector(string first_name, string last_name)
 		{
-			string cmd = $"INSERT Directors (first_name,last_name) VALUES (N'{first_name}',N'{last_name}')";
+			#region MyRegion
+			//string cmd = $"INSERT Directors (first_name,last_name) VALUES (N'{first_name}',N'{last_name}')";
+			//SqlCommand command = new SqlCommand(cmd, connection);
+			//connection.Open();
+			//command.ExecuteNonQuery();
+			//connection.Close(); 
+			#endregion
+			Insert("Directors", "first_name,last_name", $"N'{first_name}',N'{last_name}'");
+		}
+
+		public static void InsertMovies(string title, string release_date, string director)
+		{
+			Insert("Movies", "title,release_date,director", $"N'{title}','{release_date}',{director}");
+			//SqlCommand command = new SqlCommand(cmd, connection);
+			//connection.Open();
+			//command.ExecuteNonQuery();
+			//connection.Close();
+		}
+		static void Insert(string table, string columns, string values, string key = "")
+		{
+			if (key == "")
+			{
+				key = table.ToLower();
+				key = key.Remove(key.Length - 1, 1) + "_id";
+			}
+			Console.WriteLine(key);
+			string[] all_columns = columns.Split(',');
+			string[] all_values = values.Split(',');
+			string condition = "";
+			for (int i = 0; i < all_columns.Length; i++)
+			{
+				condition += $"{all_columns[i]} = {all_values[i]}";
+				if (i != all_columns.Length - 1) condition += " AND ";
+			}
+			string check_string = $"IF NOT EXISTS (SELECT {key} FROM {table} WHERE {condition})";
+			string query = $"INSERT {table}({columns}) VALUES ({values})";
+			string cmd = $"{check_string} BEGIN {query} END";
+			Console.WriteLine(cmd);
 			SqlCommand command = new SqlCommand(cmd, connection);
 			connection.Open();
 			command.ExecuteNonQuery();
 			connection.Close();
 		}
+
 	}
 }

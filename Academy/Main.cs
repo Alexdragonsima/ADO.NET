614635a3ccce4c009a5c213bba3cb12516cae869
Алексей Семионов <alehaaaaa@mail.ru>
2025-04-08 15:27:10 +0300

    Weekdays DONE

--- Academy/Main.cs
+++ Academy/Main.cs
@@ -15,16 +15,69 @@ namespace Academy
 {
 	public partial class Main : Form
 	{
+		Connector connector;
 		public Main()
 		{
 			InitializeComponent();
 
-			Connector connector = new Connector
+			connector = new Connector
 				(
 					ConfigurationManager.ConnectionStrings["DBMS_DDL"].ConnectionString
 				);
 			// dgv - dataGridview
-			dgvStudents.DataSource = connector.Select("*", "Students");
+			dgvStudents.DataSource = connector.Select
+				(
+					//"last_name,first_name,middle_name,birth_date,group_name,direction_name",
+					//"Students,Groups,Directions",
+					//"[group]=group_id AND direction_id"
+					"*","Students"
+				);
+			toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
+		}
+
+		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			switch (tabControl.SelectedIndex)
+			{
+				case 0:
+					dgvStudents.DataSource =
+						connector.Select
+						(
+						//	"last_name,first_name,middle_name,birth_date,group_name,direction_name",
+						//	"Students,Groups,Directions",
+						//	"[group]=group_id AND direction_id"
+							"*","Students"
+						);
+					toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
+					break;
+
+				case 1:
+					dgvGroups.DataSource = connector.Select
+						(
+							//"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name",
+							//"Groups,Directions",
+							//"direction=direction_id"
+							"*","Groups"
+						);
+					toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
+					break;
+
+				case 2:
+					dgvDirections.DataSource = connector.Select("*", "Directions");
+					toolStripStatusLabelCount.Text = $"Количество Направлений:{dgvDirections.RowCount - 1}.";
+					break;
+
+				case 3:
+					dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
+					toolStripStatusLabelCount.Text = $"Количество дисциплин:{dgvDisciplines.RowCount - 1}.";
+					break;
+
+				case 4:
+					dgvTeachers.DataSource = connector.Select("*", "Teachers");
+					toolStripStatusLabelCount.Text = $"Количество преподавателей:{dgvTeachers.RowCount - 1}.";
+					break;
+
+			}
 		}
 	}
 }

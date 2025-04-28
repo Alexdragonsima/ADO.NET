c4f3e13618aefc58c1cb94486d05d74183220c48
Алексей Семионов <alehaaaaa@mail.ru>
2025-04-28 14:37:55 +0300

    Parent Combo-Dependencies DONE

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -20,7 +20,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
-		public Dictionary<ComboBox, List<ComboBox>> d_dependincies;
+		public Dictionary<ComboBox, List<ComboBox>> d_children;
+		public Dictionary<ComboBox, List<ComboBox>> d_parents;
 
 		DataGridView[] tables;
 		Query[] queries = new Query[]
@@ -61,9 +62,13 @@ namespace Academy
 		{
 			InitializeComponent();
 
-			d_dependincies = new Dictionary<ComboBox, List<ComboBox>>()
+			d_children = new Dictionary<ComboBox, List<ComboBox>>()
 			{
 				{cbStudentsDirection,new List<ComboBox>(){cbStudentsDirection } }
+			};
+			d_parents = new Dictionary<ComboBox, List<ComboBox>>()
+			{
+				{cbStudentsGroup, new List<ComboBox>{cbStudentsDirection} }
 			};
 
 			tables = new DataGridView[]
@@ -115,62 +120,66 @@ namespace Academy
 			Console.WriteLine(tab_name);
 			//int i = tabControl.SelectedIndex;
 			LoadPage(tabControl.SelectedIndex);
+
+			#region SWITCH
 			/*switch (tabControl.SelectedIndex)
-			{
-				case 0:
-					dgvStudents.DataSource =
-						connector.Select
-						(
-							//"last_name,first_name,middle_name,birth_date,group_name,direction_name",
-							//"Students,Groups,Directions",
-							//"[group]=group_id AND direction_id"
-							"*", "Students"
-						);
-					toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
-					break;
+				{
+					case 0:
+						dgvStudents.DataSource =
+							connector.Select
+							(
+								//"last_name,first_name,middle_name,birth_date,group_name,direction_name",
+								//"Students,Groups,Directions",
+								//"[group]=group_id AND direction_id"
+								"*", "Students"
+							);
+						toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
+						break;
 
-				case 1:
-					dgvGroups.DataSource = connector.Select
-						(
-							"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,time,direction_name",
-							"Groups,Directions",
-							"direction=direction_id"
-						//"*", "Groups"
-						);
-					toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
-					break;
+					case 1:
+						dgvGroups.DataSource = connector.Select
+							(
+								"group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,time,direction_name",
+								"Groups,Directions",
+								"direction=direction_id"
+							//"*", "Groups"
+							);
+						toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
+						break;
 
-				case 2:
-					//dgvDirections.DataSource = connector.Select
-					//	(
-					//		//"*", "Directions"
-					//		"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(student_id) AS N'Количество студентов'",
-					//		"Students,Groups,Directions",
-					//		"[group]=group_id AND direction=direction_id",
-					//		"direction_name"
-					//	);
-					dgvDirections.DataSource = connector.Select
-						(
-							//"*", "Directions"
-							"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(student_id) AS N'Количество студентов'",
-							"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)",
-							"",
-							"direction_name"
-						);
-					toolStripStatusLabelCount.Text = $"Количество Направлений:{dgvDirections.RowCount - 1}.";
-					break;
+					case 2:
+						//dgvDirections.DataSource = connector.Select
+						//	(
+						//		//"*", "Directions"
+						//		"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(student_id) AS N'Количество студентов'",
+						//		"Students,Groups,Directions",
+						//		"[group]=group_id AND direction=direction_id",
+						//		"direction_name"
+						//	);
+						dgvDirections.DataSource = connector.Select
+							(
+								//"*", "Directions"
+								"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп' , COUNT(student_id) AS N'Количество студентов'",
+								"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)",
+								"",
+								"direction_name"
+							);
+						toolStripStatusLabelCount.Text = $"Количество Направлений:{dgvDirections.RowCount - 1}.";
+						break;
 
-				case 3:
-					dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
-					toolStripStatusLabelCount.Text = $"Количество дисциплин:{dgvDisciplines.RowCount - 1}.";
-					break;
+					case 3:
+						dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
+						toolStripStatusLabelCount.Text = $"Количество дисциплин:{dgvDisciplines.RowCount - 1}.";
+						break;
 
-				case 4:
-					dgvTeachers.DataSource = connector.Select("*", "Teachers");
-					toolStripStatusLabelCount.Text = $"Количество преподавателей:{dgvTeachers.RowCount - 1}.";
-					break;
+					case 4:
+						dgvTeachers.DataSource = connector.Select("*", "Teachers");
+						toolStripStatusLabelCount.Text = $"Количество преподавателей:{dgvTeachers.RowCount - 1}.";
+						break;
 
-			}*/
+				}*/
+			#endregion
+
 		}
 
 
@@ -204,9 +213,9 @@ namespace Academy
 			//	);
 			//cbStudentsGroup.Items.Clear();
 			//cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
-			if (d_dependincies.ContainsKey(sender as ComboBox))
+			if (d_children.ContainsKey(sender as ComboBox))
 			{
-				foreach (ComboBox cb in d_dependincies[sender as ComboBox])
+				foreach (ComboBox cb in d_children[sender as ComboBox])
 				{
 					GetDependentData(cb, sender as ComboBox);
 				}
@@ -222,9 +231,31 @@ namespace Academy
 
 			Query query = new Query(queries[tabControl.SelectedIndex]);
 			string condition =
-				(i == 0 || (sender as ComboBox).SelectedItem == null ? "" : $"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}");
+				(
+					i == 0 ||
+					(sender as ComboBox).SelectedItem == null ? "" :
+					$"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}"
+				);
+			string parent_condition = "";
+			if (d_parents.ContainsKey(sender as ComboBox))
+			{
+				foreach (ComboBox cb in d_parents[sender as ComboBox])
+				{
+					if (cb.SelectedItem != null && cb.SelectedIndex > 0)
+					{
+						string column_name = cb.Name.Substring(Array.FindLastIndex<char>(cb.Name.ToCharArray(), Char.IsUpper));
+						string parent_dictionary_name = $"d_{column_name.ToLower()}s";
+						Dictionary<string, int> parent_dictionary = this.GetType().GetField(parent_dictionary_name).GetValue(this) as Dictionary<string, int>;
+						if (parent_condition != "") parent_condition += " AND ";
+						parent_condition += $"[{column_name}]={parent_dictionary[cb.SelectedItem.ToString()]}";
+					}
+				}
+			}
+
 			if (query.Condition == "") query.Condition = condition;
 			else if (condition != "") query.Condition += $"AND {condition}";
+			if (query.Condition == "") query.Condition = parent_condition;
+			else if (parent_condition != "") query.Condition += $" AND {parent_condition}";
 			LoadPage(tabControl.SelectedIndex, query);
 		}
 		void GetDependentData(ComboBox dependent, ComboBox determinant)
@@ -241,7 +272,7 @@ namespace Academy
 				(
 					$"{dependent_root.ToLower()}_id,{dependent_root.ToLower()}_name",
 					$"{dependent_root}s,{determinant_root}s",
-					determinant.SelectedItem == null||determinant.SelectedIndex<=0 ? "" : $"{determinant_root.ToLower()}={determinant.SelectedIndex}"
+					determinant.SelectedItem == null || determinant.SelectedIndex <= 0 ? "" : $"{determinant_root.ToLower()}={determinant.SelectedIndex}"
 				);
 			foreach (KeyValuePair<string, int> d in dictionary)
 			{
@@ -250,6 +281,8 @@ namespace Academy
 
 			dependent.Items.Clear();
 			dependent.Items.AddRange(dictionary.Select(d => d.Key).ToArray());
+			dependent.Items.Insert(0, "Все");
+			dependent.SelectedIndex = 0;
 
 			Console.WriteLine("Dependent: \t" + dependent_root);
 			Console.WriteLine("Determinant: \t" + determinant_root);
